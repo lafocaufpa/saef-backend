2ad0e90f41eadace65b2afa9ae2ad386a3260976
edinaldohenriques <edinaldohenriques02@gmail.com>
2024-01-28 22:02:05 -0300

    Configuração do setup do projeto

---
+++ lib/api_constraints.rb
@@ -0,0 +1,10 @@
+class ApiConstraints 
+  de def initialize(options)
+    @version = options[:version]
+    @default = options[:default]
+  end
+
+  def matches?(req)
+    @default || req.headers['Accept'].include?("application/vnd.saef.v#{@version}")
+  end
+end
